406e1fac8268bcb225349248ac93700ca8b9f379
uchrissd <uchrissd@protonmail.com>
2019-10-21 08:13:17 -0700

    fixed the api call

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,24 @@
 //Function to build the query URL
 
-//Variable for API key
-var APIKey = "8e6cd4b0a92515f9df6154e3e1a6497a";
-
-//Grab the user search input
-var userInput = $("#city-search").val();
-var search = "q=" + userInput + '&units=imperial&appid="';
-//Variable for query URL to access API
-
-var queryURL =
-  "https://api.openweathermap.org/data/2.5/weather?" + search + APIKey;
-
-// Logging the URL so we have access to it for troubleshooting
-
-console.log("---------------\nURL: " + queryURL + "\n---------------");
-console.log(queryURL);
-
 //Click handlers
 $("#run-search").on("click", function(event) {
   event.preventDefault();
+
+  //Variable for API key
+  var APIKey = "&APPID=8e6cd4b0a92515f9df6154e3e1a6497a";
+
+  //Grab the user search input
+  var userInput = $("#city-search").val();
+  var search = "q=" + userInput + "&units=imperial";
+  //Variable for query URL to access API
+
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?" + search + APIKey;
+
+  // Logging the URL so we have access to it for troubleshooting
+
+  console.log("---------------\nURL: " + queryURL + "\n---------------");
+  console.log(queryURL);
   alert(userInput);
 
   // Here we run our AJAX call to the OpenWeatherMap API
